b34f9d13dc20e6f6bfe30273ba42214ae81c24fc
Nathanael Perraudin <nathanael.perraudin@epfl.ch>
2015-09-16 00:44:01 +0200

    version 1.6.3

--- mat2doc/php/conf.py
+++ mat2doc/php/conf.py
@@ -1,6 +1,6 @@
 fext='.php'
-urlbase='/doc'
+urlbase='https://lts2.epfl.ch/unlocbox/doc'
 urlext='.php'
 widthstr='600px'
 imagetype='png'
-includedir='../include'
+includedir='https://lts2.epfl.ch/unlocbox/include'
